80a13cf32e0f9d49d66594295c32426136b8ae0b
kido <86568833+qyukido@users.noreply.github.com>
2023-03-02 17:58:45 -0300

    Add files via upload

---
+++ Semana2/fatura.py
@@ -0,0 +1,7 @@
+cliente= input("Digite o nome do cliente: ")
+diaVencimento= input("Digite o dia de vencimento: ")
+mêsVencimento= input("Digite o mês de vencimento: ")
+valorFatura= input("Digite o valor da fatura: ")
+
+print("Olá, ", cliente)
+print("A sua fatura com vencimento em ", diaVencimento, "de", mêsVencimento, "no valor de R$", valorFatura, "está fechada.")
