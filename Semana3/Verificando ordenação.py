9444632de361d9b8d36f71f022d4d29584ab11e9
yukioktk <86568833+yukioktk@users.noreply.github.com>
2023-06-02 12:50:44 -0300

    Add files via upload

---
+++ Semana3/Verificando ordenação.py
@@ -0,0 +1,8 @@
+num1 = int(input("Insira o primeiro número: "))
+num2 = int(input("Insira o segundo número: "))
+num3 = int(input("Insira o terceiro número: "))
+
+if num1 < num2 < num3:
+  print("crescente")
+else:
+  print("não está em ordem crescente")
